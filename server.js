981cdbb69b11a5009317d89081ce9a8f48ce57d4
vikecrub-k <vikecrub@gmail.com>
2025-12-16 23:34:53 +0800

    n

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
+require('dotenv').config();
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
